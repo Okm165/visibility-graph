15092abae9516ad3bf34ad9a296b9554d05483f8
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-01-02 18:13:56 +0100

    visualisation

--- visibility_graph.py
+++ visibility_graph.py
@@ -1,11 +1,16 @@
 from visible_vertices import *
 from shortest_path import *
+from utilities import *
 
 
 class VisibilityGraph:
-    def __init__(self, graph: Graph) -> None:
+    def __init__(self, graph: Graph, redlines, start, end) -> None:
         self.graph = graph  # holds original obstacle graph
         self.visibility_graph = Graph()  # holds visibility connections
+        self.redlines = redlines
+        self.scenes = []
+        self.start = start
+        self.end = end
 
     def visible_from(self, O: Vertex) -> list:
         """return list of edges that represent visibility rays (to be added into visibility graph)"""
@@ -23,6 +28,20 @@ class VisibilityGraph:
         for vert in verts:
             for edge in self.visible_from(vert):
                 self.visibility_graph.add_edge(edge)
+            self.scenes.append(Scene(
+                points = 
+                [PointsCollection([(self.start.x, self.start.y), (self.end.x, self.end.y)], color = "green"),
+                PointsCollection([(vert.x, vert.y)], color = "purple")], 
+                lines = 
+                [graph_to_linesCollection(self.union(), "grey"), self.redlines] ))
 
     def union(self):
         return self.graph | self.visibility_graph
+
+    def shortest_path_scene(self):
+        self.scenes.append(Scene(
+            points = [PointsCollection([(self.start.x, self.start.y), (self.end.x, self.end.y)],
+            color = "green")] , 
+            lines = [graph_to_linesCollection(self.union(), "grey"), 
+            self.redlines, 
+            edges_to_linesCollection(self.shortest_path(self.start, self.end), "green")]))
